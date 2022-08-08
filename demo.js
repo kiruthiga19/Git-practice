281eda9348b6cac7e1c2dccbd1f1caa600c13d9a
kiruthiga19 <99905519+kiruthiga19@users.noreply.github.com>
2022-08-08 10:11:49 +0530

    demo javascript class

---
+++ demo.js
@@ -0,0 +1,13 @@
+// variable 
+// var names="kiruthiga";
+// console.log(names);
+// let
+// var
+// const 
+// let number=19;
+// number=20;
+// console.log(number);
+const firstName=30;
+console.log(firstName);
+// if
+
